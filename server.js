97b7eb7cc1a38020dcd6f7a360da99f4a80512d5
Dara Carolan <dara.carolan@accenture.com>
2017-01-08 17:44:41 +0000

    adding frontend to index page

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.get('/dara', function (req, res) {
   res.sendFile(__dirname + '/dara.html');
 });
 
+app.get('/about', function (req, res) {
+  res.sendFile(__dirname + '/about.html');
+});
+
+//serve static files
+app.use(express.static('public'));
+
 //start up express
 app.listen(port, function(err) {
   if (err) {
