0522d6c2add4ab00413e589406285c58bde1507c
Dara Carolan <dara.carolan@accenture.com>
2017-02-16 08:52:22 +0000

    simple adding coderbyte

--- 20-12-2016-dara.js
+++
@@ -1,7 +0,0 @@
-function longestWordSearch(str) {
-  let strArray = str.split(' ');
-  let longest = strArray.sort(function (a, b) { return b.length - a.length; })[0];
-  alert(longest);
-}
-
-longestWordSearch('This is marmalade jammalade');
